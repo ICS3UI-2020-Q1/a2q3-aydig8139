f4d0da2cb216a20a4f5566a888119ad62dba49fc
aydig8139 <aydig8139@wrdsb.ca>
2020-09-15 17:24:41 +0000

    completed so that it lists two numbers in ascending order

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Types out two numbers in ascennding order
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user for two integers
+    System.out.println("Please enter an integer:");
+    int firstInt = input.nextInt();
+
+    System.out.println("Please enter another integer:");
+    int secondInt = input.nextInt();
+
+    if (firstInt > secondInt) {
+      System.out.println("Your numbers in ascending order are " + secondInt + ", " + firstInt);
+    } else if (firstInt == secondInt) {
+      System.out.println("Your numbers in ascending order are " + firstInt + ", " + secondInt);
+    } else {
+      System.out.println("Your numbers in ascending order are " + firstInt + ", " + secondInt);
+    }
     
   }
 }
